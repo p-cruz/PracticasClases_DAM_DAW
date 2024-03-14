86d201527dc7783a297f0f7baf6991ffa2ca0aaa
Paula <paulacruz@iesgregorioprieto.com>
2024-03-14 10:01:20 +0100

    node con provincias y municipios

--- practicasDAW/4_JSON/NODEJS1/indexfs.js
+++ practicasDAW/4_JSON/NODEJS1/indexfs.js
@@ -22,14 +22,25 @@ var server = http.createServer(function(req, res){
                 res.setHeader('Content-Type', 'text/css');
                 res.end(data);            }
         });
+    }else if (req.url=='/provincias.json'){
+        fs.readFile('web/provincias.json','utf8',function(err,data){
+            if (err){
+                console.log('Error: '+err);
+            }else{
+                res.setHeader('Content-Type', 'application/json');
+                res.end(data);
+            }
+        });
+    }else if (req.url=='/municipios.json'){
+        fs.readFile('web/municipios.json','utf8',function(err,data){
+            if (err){
+                console.log('Error: '+err);
+            }else{
+                res.setHeader('Content-Type', 'application/json');
+                res.end(data);
+            }
+        });
     }
-    /*else if (req.url == '/uno'){
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify({nombre:"Juan",edad:30}));
-    }else if (req.url == '/dos'){
-        res.setHeader('Content-Type', 'application/json');
-        res.end('{"nombre":"Ana","edad":25}');
-    }*/
 });
 console.log('Servidor escuchando por el puerto 3000...')
 server.listen(3000);
