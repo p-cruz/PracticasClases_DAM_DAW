78123feff76db9f88391b887031698cbaa2594a8
Paula <paulacruz@iesgregorioprieto.com>
2024-04-05 09:40:17 +0200

    plsql2

--- practicasDAW/BasesDeDatos/plsql/bloqueAnonimo1.sql
+++ practicasDAW/BasesDeDatos/plsql/bloqueAnonimo1.sql
@@ -11,10 +11,10 @@ begin
     where  oficio='VENDEDOR' and comision>500;
 
     if (cantidad>3) then
-        dbms_output.put_line('Hay más de 3 empleados con esas condiciones, se deshace la operación de subida de salario');
+        dbms_output.put_line('Hay mas de 3 empleados con esas condiciones, se deshace la operacion de subida de salario');
         rollback;
     else
-        dbms_output.put_line('Se sube el salario de los vendedores con más de 600€ de comisión, ya que no hay más de 3');
+        dbms_output.put_line('Se sube el salario de los vendedores con mas de 500 euros de comision, ya que solo hay '|| cantidad ||' empleados con esas condiciones');
     end if;
 end;
 /
